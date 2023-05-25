771da157057abf784658573ade94a118174f3227
Nattrin <natalia.rin08@gmail.com>
2023-05-25 18:39:57 -0500

    Info de pokeapi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'infoexterna';
+  articulos: any;
+  constructor(private http: HttpClient) { }
+  ngOnInit():void {
+    let url = `https://pokeapi.co/api/v2/pokemon/ditto`; 
+    this.http.get(url).subscribe(
+      (response:any) => { this.articulos = response})
+  }
 }
